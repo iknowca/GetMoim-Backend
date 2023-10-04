450f169354f294a61f1524f7112cca4ed95ed711
iknowca <iknowca@gmail.com>
2023-10-04 21:00:21 +0900

    updata: [김원기] CORS 설정 추가 [WMB-1]

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://127.0.0.1:8080", //vue는 허용하겠다는 소리, *로 써도 되지만 전부 다라는 이야기
                                 "http://localhost:8080",
                                 "http://192.168.20.3:8080",
-                                "http://221.149.244.32:8080"
+                                "http://221.149.244.32:8080",
+                                "http://get-moim.shop:8080"
                         )
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
